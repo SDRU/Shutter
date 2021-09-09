575b057d877c21cfb1ce4a003f4b6456e63764ef
Sandra Drusova <sandra.drusova@unibas.ch>
2021-09-09 13:49:10 +0200

    Functions are integrated to kinesis file

--- ThorlabsShutter.py
+++ ThorlabsShutter.py
@@ -7,35 +7,44 @@ Created on Tue Jul 27 16:30:47 2021
 
 from pylablib.devices import Thorlabs
 import time
-import struct
-from ThorlabsShutterFunctions import *
 
 
 # USER DEFINED PARAMETERS
-on_time = int(5e3) # in 1 ms steps
-off_time = int(5e3)
-num_cycles = 2
+on_time = 5e3 # in 1 ms steps
+off_time = 5e3
+num_cycles = 3
 
+devices = Thorlabs.list_kinesis_devices()
+print('Devices found: ',devices)
 
-print('Device found: ',Thorlabs.list_kinesis_devices())
+class DeviceNotConnectedError(Exception):
+    pass
 
 try:
-
-    shutter = Thorlabs.kinesis.BasicKinesisDevice("68000959")
-    shutter.open()
-    # on_time, off_time, num_cycles = get_cycle_params(shutter)
     
-    set_operating_mode(shutter,2)
-    set_cycle_params(shutter, on_time, off_time, num_cycles)
+    for device in devices:
+        serial_nr = device[0]
+        if serial_nr.startswith("6"):
+            shutter = Thorlabs.kinesis.KinesisDevice(serial_nr)
+            shutter.open()
+            
+        else:
+            raise DeviceNotConnectedError
+    on_time, off_time, num_cycles = shutter.get_cycle_params()
     
-    # open shutter once
-    shutter_on(shutter)
-    # time.sleep(2)
-    # shutter_off(shutter)
-
+    shutter.set_operating_mode(mode=3)
+    shutter.set_cycle_params(on_time=on_time, off_time=off_time, num_cycles=num_cycles)
     
-
+    # # open shutter once
+    shutter.shutter_open()
+    
+    time.sleep(35)
+        
+    
     shutter.close()
+    
+except DeviceNotConnectedError:
+    print("No shutter connected")
 
 except KeyboardInterrupt:
     shutter.close()
@@ -44,3 +53,5 @@ except KeyboardInterrupt:
 except:
     shutter.close()
     print('Sth not working here')
+    
+    
